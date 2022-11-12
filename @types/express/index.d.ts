94df5ccb4accba955962b9d77b73673a1080180d
AlexandraZolotykhina <87070723+AlexandraZolotykhina@users.noreply.github.com>
2022-11-12 16:00:04 +0300

    fix: Исправить типизацию Request

---
+++ @types/express/index.d.ts
@@ -0,0 +1,11 @@
+import { ObjectId } from 'mongoose';
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  namespace Express {
+    // eslint-disable-next-line no-unused-vars
+    interface Request {
+      user: { _id: string | ObjectId }
+    }
+  }
+}

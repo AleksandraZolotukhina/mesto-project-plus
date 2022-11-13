d53481cfcf530b5d9e0e190313a822b18a965812
AlexandraZolotykhina <87070723+AlexandraZolotykhina@users.noreply.github.com>
2022-11-13 20:04:19 +0300

    feat: Добавить ERROR_AUTHORIZATED в middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,18 +1,19 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+import { ERROR_AUTHORIZATED } from '../utils/error';
 
 // eslint-disable-next-line consistent-return
 export default (req: Request, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    return res.status(ERROR_AUTHORIZATED).send({ message: 'Необходима авторизация' });
   }
   let payload;
   try {
     payload = jwt.verify(authorization.replace('Bearer ', ''), 'super-strong-secret');
   } catch (err) {
     return res
-      .status(401)
+      .status(ERROR_AUTHORIZATED)
       .send({ message: 'Необходима авторизация' });
   }
   req.user = payload;

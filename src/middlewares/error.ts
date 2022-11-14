269efe1bb28e4a7d3d13baa8f8ae619f340c05e8
AlexandraZolotykhina <sashazolotykhina@mail.ru>
2022-11-14 15:55:53 +0300

    feat: Создать middleware ошибок

---
+++ src/middlewares/error.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default (
+  err: { statusCode: number, message: string },
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+
+  next();
+};

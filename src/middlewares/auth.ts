a979e9308a5388aefc361a3b0d025e6affd5a741
AlexandraZolotykhina <sashazolotykhina@mail.ru>
2022-11-14 15:57:21 +0300

    fix: Исправить типизацию у req.user

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,21 +1,25 @@
-import jwt from 'jsonwebtoken';
+import { JwtPayload, verify } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { ERROR_AUTHORIZATED } from '../utils/error';
 
 // eslint-disable-next-line consistent-return
 export default (req: Request, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
+
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res.status(ERROR_AUTHORIZATED).send({ message: 'Необходима авторизация' });
   }
+
   let payload;
+
   try {
-    payload = jwt.verify(authorization.replace('Bearer ', ''), 'super-strong-secret');
+    payload = verify(authorization.replace('Bearer ', ''), 'super-strong-secret');
   } catch (err) {
     return res
       .status(ERROR_AUTHORIZATED)
       .send({ message: 'Необходима авторизация' });
   }
-  req.user = payload;
+
+  req.user = payload as {_id: JwtPayload};
   next();
 };

3c7a009a20be2f6f9338796f49d8f3f7e8ab096a
AlexandraZolotykhina <87070723+AlexandraZolotykhina@users.noreply.github.com>
2022-11-18 13:27:41 +0300

    feat: подключить валидацию для getUser

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -9,13 +9,14 @@ import {
 import {
   updateUserValidation,
   updateUserAvatarValidation,
+  getUserValidation,
 } from '../utils/validation';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
 userRouter.get('/me', getCurrentUser);
-userRouter.get('/:userId', getUser);
+userRouter.get('/:userId', getUserValidation, getUser);
 userRouter.patch('/me', updateUserValidation, updateUser);
 userRouter.patch('/me/avatar', updateUserAvatarValidation, updateUserAvatar);
 
